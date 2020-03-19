22d65ed56ae9914112cf36ac5eb44b8fed758a4a
Jason <jayjscotto@gmail.com>
2020-03-18 20:55:22 -0400

    updating blog modal styling

--- client/src/Components/BlogModal.js
+++ client/src/Components/BlogModal.js
@@ -1,15 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import API from '../Utils/API';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+  blogPost: {
+    margin: '2em'
+  }
+}));
 
 
 const BlogModal = props => {
   const [ blogPosts, setBlogPosts ] = useState([]);
-
+  const classes = useStyles();
 
   useEffect(() => {
     API.getBlogPosts().then(posts => {
-      console.log(posts.data)
-      setBlogPosts(posts.data)
+      setBlogPosts(posts.data);
     });
   }, [])
 
@@ -18,11 +24,11 @@ const BlogModal = props => {
     <>
       {blogPosts ? (blogPosts.map(post => {
         return (
-          <>
+          <div className={classes.blogPost}>
             <h1>{post.title}</h1>
-            <p>{post.post}</p>
+            <p style={{textAlign: 'justify'}}>{post.post}</p>
             <hr></hr>
-          </>
+          </div>
         )
       })) : (<></>)}
     </>
